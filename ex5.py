ff3bebdb53c51b968b9e9fae8c1092a1a7d2049b
Jason Huang <chses910372@gmail.com>
2017-02-19 19:39:50 +0800

    update ex5.py

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Jason'
+my_age = 22
+my_height = 178
+my_weight = 92
+my_eyes = 'black'
+my_teeth = 'Yellow'
+my_hair = 'black'
+
+print "Let's talk about %s" % my_name
+print "He's %d cm tall" % my_height
+print "He's %d kgs heavy" % my_weight
+print "Actually that's not too heavy"
+print "He is got %s eyes and %s hair" %(my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee."% my_teeth
+
+print "If I add %d, %d, and %d I get %d."%(my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+
